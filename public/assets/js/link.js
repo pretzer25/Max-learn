411bec0f70ec72633787f388da34cb194e2f11c6
n mnn <153140996+pretzer25@users.noreply.github.com>
2024-02-07 07:54:44 -0600

    Update link.js

--- public/assets/js/link.js
+++ public/assets/js/link.js
@@ -1,14 +1,3 @@
-
-    // <button onclick="sendLink('https://example.com')">Send Link</button>
-
-        // Function to add a random letter between each letter in the link
-        function addRandomLetters(link) {
-            return link.split('').map(char => char + String.fromCharCode(65 + Math.floor(Math.random() * 26))).join('');
-        }
-
-        // Function to send link to go.html
-        function sendLink(link) {
-            const modifiedLink = addRandomLetters(link);
-            // Open go.html in a new window and pass the modified link as a query parameter
-            window.open(`go.html?link=${encodeURIComponent(modifiedLink)}`, '_blank');
-        }
+  window.addEventListener('beforeunload', (event) => {
+    event.returnValue = 'This part is useless but I still added it...';
+  }

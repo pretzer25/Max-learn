6a5ad713d0a1f9ca22d062f6f8fe294fff803091
n mnn <153140996+pretzer25@users.noreply.github.com>
2024-02-11 20:36:12 -0600

    Delete public/scripts directory

--- public/scripts/nowggadblockremove.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-    var div=document.getElementsByTagName("div");
-    for (var i=0; i<div.length;i++){
-        if (div[i].className.includes("ad-blocker")){
-            div[i].style.display="none";
-        }
-    }
-},0);
